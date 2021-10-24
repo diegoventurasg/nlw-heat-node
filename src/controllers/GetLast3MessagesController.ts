bab7ad673f8beff5ff546fd6dfd0155a5cc3eed1
Diego Ventura <diegoventurasg@gmail.com>
2021-10-24 11:16:35 -0300

    refactor: remove unused code

--- src/controllers/GetLast3MessagesController.ts
+++ src/controllers/GetLast3MessagesController.ts
@@ -4,15 +4,11 @@ import { GetLast3MessagesService } from "../services/GetLast3MessagesService";
 
 class GetLast3MessagesController {
     async handle(request: Request, response: Response) {
-        const { message } = request.body;
-        const { user_id } = request;
-
         const service = new GetLast3MessagesService();
 
         const result = await service.execute();
 
         return response.json(result);
-
     }
 
 }
